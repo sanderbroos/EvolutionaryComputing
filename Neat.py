ba468740ceca560d22712d8bc32cdf7e39e48b64
Shaojie <shaojie483@gmail.com>
2022-10-15 21:06:24 +0200

    Modified for multiple run

--- Neat.py
+++ Neat.py
@@ -8,6 +8,7 @@ Below code makes use of code from Neat package https://github.com/CodeReclaimers
 '''
 
 import sys
+from pathlib import Path
 
 sys.path.insert(0, 'evoman')
 from environment import Environment
@@ -34,9 +35,10 @@ if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 # enemy_id
-enemy_id = [1, 5, 6]
-NGEN = 30
-runs = 1  # needs to be changed to 10
+enemy_group = [1, 2, 5]
+NGEN = 50
+# run_id
+runs = [1]  # needs to be changed to 10
 run_mode = 'train'  # train or test
 
 # Determine path to configuration file. This path manipulation is
@@ -52,7 +54,7 @@ config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
 # initializes environment with ai player using random controller, playing against static enemy
 # create an Evoman environment
 env = Environment(experiment_name=experiment_name,
-                  enemies=enemy_id,
+                  enemies=enemy_group,
                   playermode="ai",
                   player_controller=player_controller(config),
                   enemymode="static",
@@ -74,14 +76,17 @@ class ParallelEvaluator(object):
         self.timeout = timeout
         self.pool = Pool(num_workers)
 
+        self.genfitnessv = []
+        self.gengainv = []
+
     def __del__(self):
         self.pool.close()  # should this be terminate?
         self.pool.join()
 
     def evaluate(self, genomes, config):
         jobs = []
-        fitnessvalues = []
-        gainvalues = []
+        fitness_values = []
+        gain_values = []
         for ignored_genome_id, genome in genomes:
             jobs.append(self.pool.apply_async(self.eval_function, (genome, config)))
 
@@ -89,33 +94,46 @@ class ParallelEvaluator(object):
         for job, (ignored_genome_id, genome) in zip(jobs, genomes):
             results = job.get(timeout=self.timeout)
             genome.fitness = results[0]
-            gainvalues.append(results[1] - results[2])
-            fitnessvalues.append(results[0])
+            gain_values.append(results[1] - results[2])
+            fitness_values.append(results[0])
 
-        genfitnessv.append(fitnessvalues)
-        gengainv.append(gainvalues)
+        self.genfitnessv.append(fitness_values)
+        self.gengainv.append(gain_values)
+
+    def draw_graph_for_run(self):
+        fit_averages = [np.mean(gen) for gen in self.genfitnessv]
+        fit_max = [max(gen) for gen in self.genfitnessv]
+        gain_averages = [np.mean(gen) for gen in self.gengainv]
+        gain_max = [max(gen) for gen in self.gengainv]
+        plt.plot(fit_averages, 'b', label="fitmean")
+        plt.plot(fit_max, 'b--', label="fitmax")
+        plt.title("Neat Fitness Results")
+        plt.xlabel("Generations")
+        plt.ylabel("Fitness")
+        plt.show()
+        plt.plot(gain_averages, 'r', label="gainmean")
+        plt.plot(gain_max, 'r--', label="gainmax")
+        plt.title("Neat Gain Results")
+        plt.xlabel("Generations")
+        plt.ylabel("Gain")
+        plt.show()
+
+    def save_result_for_run(self, path):
+
+        with open(os.path.join(path, 'genfitnessv'), 'wb') as f:
+            pickle.dump(self.genfitnessv, f)
+        with open(os.path.join(path, 'gengainv'), 'wb') as f:
+            pickle.dump(self.gengainv, f)
 
 
-def eval_genomes(genomes, config):
-    fitnessvalues = []
-    gainvalues = []
-    for genome_id, g in genomes:
-        results = env.play(pcont=g)
-        # print('fitness, playerlife, enemylife, time:',results)
-        g.fitness = results[0]
-        gainvalues.append(results[1] - results[2])
-        fitnessvalues.append(results[0])
-    genfitnessv.append(fitnessvalues)
-    gengainv.append(gainvalues)
+def eval_genome(genome, config):
+    return env.play(pcont=genome)
 
 
-def eval_genome_2(genome, config):
-    results = env.play(pcont=genome)
+def run(run_id):
+    path = f'neat_result/{",".join(str(enemy) for enemy in enemy_group)}/run_{run_id}'
+    Path(path).mkdir(parents=True, exist_ok=True)
 
-    return results
-
-
-def run():
     # Create the population, which is the top-level object for a NEAT run.
     # p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-9') # if want to begin at checkpoint
     p = neat.Population(config)
@@ -126,15 +144,18 @@ def run():
     p.add_reporter(stats)
     # p.add_reporter(neat.Checkpointer(5)) # creates checkpoints
 
-    pe = ParallelEvaluator(multiprocessing.cpu_count(), eval_genome_2)
+    pe = ParallelEvaluator(multiprocessing.cpu_count(), eval_genome)
 
     # Run for up to NGEN generations.
     winner = p.run(pe.evaluate, NGEN)
     # Display the winning genome.
     # print('\nBest genome:\n{!s}'.format(winner))
     # Save the winner.
-    with open('winner-Neat', 'wb') as f:
+
+    with open(os.path.join(path, 'winner'), 'wb') as f:
         pickle.dump(winner, f)
+
+    pe.save_result_for_run(path)
 
 
 if __name__ == '__main__':
@@ -144,8 +165,7 @@ if __name__ == '__main__':
         individual_gains = []
         for en in range(1, 9):
             # Disable the visulization for training modes, increasing training speed
-            os.environ["SDL_VIDEODRIVER"] = "dummy"
-            bsol = pd.read_pickle(r'winner-Neat')
+            bsol = pd.read_pickle(r'winner')
             print('\n RUNNING SAVED BEST SOLUTION \n')
             env.update_parameter('enemies', [en])
             env.update_parameter('multiplemode', 'no')
@@ -155,26 +175,7 @@ if __name__ == '__main__':
         np.savetxt(f'Neat-individual_gain', individual_gains)
         sys.exit(0)
 
-    genfitnessv = []
-    gengainv = []
-    for _ in range(runs):
-        run()
+    for run_id in runs:
+        run(run_id)
 
-    # genfitnessv variable currently isn't set up for multiple runs 
-
-    fit_averages = [np.mean(gen) for gen in genfitnessv]
-    fit_max = [max(gen) for gen in genfitnessv]
-    gain_averages = [np.mean(gen) for gen in gengainv]
-    gain_max = [max(gen) for gen in gengainv]
-    plt.plot(fit_averages, 'b', label="fitmean")
-    plt.plot(fit_max, 'b--', label="fitmax")
-    plt.title("Neat Fitness Results")
-    plt.xlabel("Generations")
-    plt.ylabel("Fitness")
-    plt.show()
-    plt.plot(gain_averages, 'r', label="gainmean")
-    plt.plot(gain_max, 'r--', label="gainmax")
-    plt.title("Neat Gain Results")
-    plt.xlabel("Generations")
-    plt.ylabel("Gain")
-    plt.show()
+    # genfitnessv variable currently isn't set up for multiple runs
